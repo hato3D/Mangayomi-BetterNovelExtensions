d451c5c07e3a3cb4211247f8dd97ccca8c14ada6
hato3D <ian.lamorre@hotmail.com>
2025-09-15 02:35:45 -0300

    Update allnovel.js

--- javascript/novel/src/en/allnovel.js
+++ javascript/novel/src/en/allnovel.js
@@ -4,7 +4,7 @@ const mangayomiSources = [{
   "baseUrl": "https://allnovel.org",
   "apiUrl": "",
   "iconUrl":
-    "https://github.com/hato3D/Mangayomi-BetterNovelExtensions/blob/main/images/AllNovel.png",
+    "https://raw.githubusercontent.com/hato3D/Mangayomi-BetterNovelExtensions/refs/heads/main/images/AllNovel.png",
   "typeSource": "single",
   "itemType": 2, // Use 0 for manga, 1 for anime and 2 for novel
   "version": "1.0.0",
