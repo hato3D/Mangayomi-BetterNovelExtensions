b9035649de75e8fd6314723458bcf67a7d326999
hato3D <ian.lamorre@hotmail.com>
2025-09-15 02:31:27 -0300

    Create wuxiabox.js

---
+++ javascript/novel/src/en/wuxiabox.js
@@ -0,0 +1,55 @@
+const mangayomiSources = [{
+  "name": "WuxiaBox",
+  "lang": "en",
+  "baseUrl": "https://www.wuxiabox.com",
+  "apiUrl": "",
+  "iconUrl":
+    "https://github.com/hato3D/Mangayomi-BetterNovelExtensions/blob/main/images/WuxiaBox.png",
+  "typeSource": "single",
+  "itemType": 2, // Use 0 for manga, 1 for anime and 2 for novel
+  "version": "1.0.0",
+  "dateFormat": "",
+  "dateFormatLocale": "",
+  "pkgPath": "novel/src/en/wuxiabox.js",
+  "hasCloudflare": false
+}];
+
+class DefaultExtension extends MProvider {
+  getHeaders(url) {
+      throw new Error("getHeaders not implemented");
+  }
+  async getPopular(page) {
+      throw new Error("getPopular not implemented");
+  }
+  async getLatestUpdates(page) {
+      throw new Error("getLatestUpdates not implemented");
+  }
+  async search(query, page, filters) {
+      throw new Error("search not implemented");
+  }
+  async getDetail(url) {
+      throw new Error("getDetail not implemented");
+  }
+  // For novel html content
+  async getHtmlContent(url) {
+      throw new Error("getHtmlContent not implemented");
+  }
+  // Clean html up for reader
+  async cleanHtmlContent(html) {
+      throw new Error("cleanHtmlContent not implemented");
+  }
+  // For anime episode video list
+  async getVideoList(url) {
+      throw new Error("getVideoList not implemented");
+  }
+  // For manga chapter pages
+  async getPageList(url) {
+      throw new Error("getPageList not implemented");
+  }
+  getFilterList() {
+      throw new Error("getFilterList not implemented");
+  }
+  getSourcePreferences() {
+      throw new Error("getSourcePreferences not implemented");
+  }
+}
